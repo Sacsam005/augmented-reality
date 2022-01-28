e9fcc59b6030f2661918543d925a077ff2581e8c
Sachin Samal <88958992+Sacsam005@users.noreply.github.com>
2022-01-28 00:15:28 -0600

    Update main.js

--- main.js
+++ main.js
@@ -6,15 +6,15 @@ const btnThree = document.getElementById('three');
 
 btnOne.addEventListener('click', function stop() {
     modelViewer.animationName = "none";
-    textBox.innerHTML = "<h1>Binito is tired !!! Lemme stop !!! </h1>";
+    textBox.innerHTML = "<h1>Binito is tired!!! Lemme stop !!! </h1>";
 })
 
 btnTwo.addEventListener('click', function run() {
     modelViewer.animationName = "Running";
-    textBox.innerHTML = "<h1>Hello guys, Binito is running now !!! </h1>"
+    textBox.innerHTML = "<h1>Hello guys, Binito is running now!!! </h1>"
 })
 
 btnThree.addEventListener('click', function wave() {
     modelViewer.animationName = "Wave";
-    textBox.innerHTML = "<h1>Hello guys, I am Binito </h1>";
+    textBox.innerHTML = "<h1>Hello guys, I am Binito</h1>";
 })
